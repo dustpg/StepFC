de71d83462aa3c5e73c46af88ec0379b330a0f17
Dust Loong <dustpg@gmail.com>
2018-08-19 06:56:18 +0800

    step 0

---
+++ step0/main.c
@@ -0,0 +1,20 @@
+﻿#include "sfc_famicom.h"
+#include <stdio.h>
+
+/// <summary>
+/// 应用程序入口
+/// </summary>
+/// <returns></returns>
+int main() {
+    sfc_famicom_t famicom;
+    sfc_famicom_init(&famicom, NULL, NULL);
+    printf(
+        "ROM: PRG-RPM: %d x 16kb   CHR-ROM %d x 8kb   Mapper: %03d\n", 
+        (int)famicom.rom_info.count_prgrom16kb,
+        (int)famicom.rom_info.count_chrrom_8kb,
+        (int)famicom.rom_info.mapper_number
+    );
+    getchar();
+    sfc_famicom_uninit(&famicom);
+    return 0;
+}
---
+++ step0/sfc_famicom.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+// License: MIT  http://opensource.org/licenses/MIT
+// Author: dustpg   mailto:dustpg@gmail.com
+
+#include "sfc_rom.h"
+#include "sfc_code.h"
+
+/// <summary>
+/// StepFC扩展接口
+/// </summary>
+typedef struct {
+    // ROM 加载器读取信息
+    sfc_ecode(*load_rom)(void*, sfc_rom_info_t*);
+    // ROM 加载器卸载
+    sfc_ecode(*free_rom)(void*, sfc_rom_info_t*);
+
+} sfc_interface_t;
+
+
+
+/// <summary>
+/// FC 模拟器主体
+/// </summary>
+typedef struct {
+    // 参数
+    void*               argument;
+    // 接口
+    sfc_interface_t     interfaces;
+    // ROM 信息
+    sfc_rom_info_t      rom_info;
+
+} sfc_famicom_t;
+
+
+// 初始化
+sfc_ecode sfc_famicom_init(
+    sfc_famicom_t* famicom,
+    void* argument,
+    const sfc_interface_t* interfaces
+);
+
+// 反初始化
+void sfc_famicom_uninit(sfc_famicom_t*);
+
+
